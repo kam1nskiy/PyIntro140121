e9c003d0c7d542611a6774491518ff2b832442cd
kaminskyi <vladimir_kaminskiy@ukr.net>
2021-01-30 11:27:52 +0200

    upload hw2

---
+++ hw2.py
@@ -0,0 +1,31 @@
+value = input("Введи число: ")
+value = int(value)
+if value % 5 == 0:
+    print (value * 2)
+else:
+    print(0)
+
+###############################
+
+# month = input("Введи номер месяца: ")
+# month = int(month)
+# if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+#     print(31)
+# elif month == 4 or month == 6 or month == 9 or month == 11:
+#     print(30)
+# elif month == 2:
+#     print(28)
+# else:
+#     print("Неправильно введен номер месяц")
+
+###############################
+
+# month = input("Введи название месяца с большой буквы: ")
+# if month == "Январь" or month == "Март" or month == "Май" or month == "Июль" or month == "Август" or month == "Октябрь" or month == "Декабрь":
+#     print(31)
+# elif month == "Апрель" or month == "Июнь" or month == "Сентябрь" or month == "Ноябрь":
+#     print(30)
+# elif month == "Февраль":
+#     print(28)
+# else:
+#     print("Проверь написание месяца, введите с заглавной буквы ")
